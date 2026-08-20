3bea8c8ad689a725d1305eac4f2e84ff858ec46b
Jithu21 <hariharanjithu@Hariharans-Laptop.local>
2026-08-20 18:08:01 +0530

    unnecessary python script

--- script2.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Enter your first number: "))
-num2 = int(input("Enter your second number: "))
-num3 = int(input("Enter your third number: "))
-if num1 > num2 and num1 < num3:
-    print("num1 is the second largest number")
-if num2 > num1 and num2 < num3:
-    print("num2 is the second largest number")
-if num3 > num1 and num3 < num2:
-    print("num3 is the second largest number")
